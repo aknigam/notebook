f736df32fe3603dda4392ea08809c02a01cb829b
NIGAM, ANAND <a.nigam@cvent.com>
2017-11-03 11:31:36 +0530

    Webnotes support added

--- models/model.go
+++ models/model.go
@@ -1,27 +1,5 @@
 package models
 
-type Customer struct {
-	Id           int    `json:"id"`
-	Name         string `json:"name"`
-	MobileNumber int    `json:"mobile"`
-	Address      string `json:"address"`
-}
-
-type Vendor struct {
-	Id      int    `json:"id"`
-	Name    string `json:"name"`
-	Owner   string `json:"owner"`
-	Address string `json:"address"`
-}
-
-type Order struct {
-	Id           int    `json:"id"`
-	CustomerId   int    `json:"customerId"`
-	VendorId     int    `json:"vendorId"`
-	OrderDetails string `json:"orderDetails"`
-	Status       string `json:"status"`
-}
-
 type NoteSnippet struct {
 	Id      int    `json:"id"`
 	PageUrl string `json:"pageUrl"`
